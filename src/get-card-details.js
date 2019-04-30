3d9500f3687a22f8847aa4ae24f1e221458ce95a
Jens Östlund <jostlund@gmail.com>
2019-04-30 12:02:53 +0200

    Make Puppeteer run on Heroku as per:
    https://github.com/GoogleChrome/puppeteer/issues/758

--- src/get-card-details.js
+++ src/get-card-details.js
@@ -17,7 +17,9 @@ module.exports = async () => {
       }`
     );
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     await page.goto(`${config.SERVICE_URL}/adyen`);
